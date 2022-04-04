34f114e1811f004e8ac5ef7bec4f741ee5195d78
malurmaia <marialuisaribeiromaia@gmail.com>
2022-04-04 19:34:07 -0300

    indo pro pc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Carousel, Container, Nav, Navbar } from 'react-bootstrap'
 import Logo from './assets/img/LOGO.png'
 import Hero from './assets/img/Group-3.jpg'
+import Comofunciona from './assets/img/Comofunciona.png'
+import notebook from './assets/img/computador-portatil.png'
+import calendario from './assets/img/timetable.png'
+import chat from './assets/img/chat.png'
+import group4 from './assets/img/group4.png'
+
 function Header () {
   return (
     <header>
@@ -17,9 +23,71 @@ function Header () {
               <Nav.Link href='/Beneficios'>Benefícios</Nav.Link>
             </Nav>
           </Navbar.Collapse>
+          {/* <button className='login'href='/'>Login</button> */}
         </Container>
       </Navbar>
+      
     </header>
+  )
+}
+function Comofuncionasection () {
+  return (
+    <section className='howitworks'>
+      <img src={Comofunciona} alt='como funciona e dois carrinhos rosa' width={200} height={100} className='img-fluid comofunciona'/>
+      <Carousel>
+        <Carousel.Item>
+          <img src={notebook} alt='notebook com imagem de um bebe' width={23} height={40} className='img-fluid d-block w-50 notebook'/>
+          <Carousel.Caption>
+            <p>
+            Veja as vantagens e características de cada plano e inscreva-se no que melhor se encaixa ao seu perfil
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={calendario} alt='calendário' className='img-fluid d-block w-50 calendario'/>
+          <Carousel.Caption>
+          <p>
+          Receba mensalmente uma malinha surpresa com os itens essenciais da fase que seu bebê se encontra e uma ajudinha extra para a mamãe.
+          </p>
+          </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={chat} alt='Balão de conversa azul,verde, amarelo com estrela e branco com esclamação vermelha.' className='img-fluid d-block w-50 chat'/>
+            <Carousel.Caption>
+              <p>
+              Amplie sua imersão navegando nos fóruns e conteúdos exclusivos junto com outras mamães
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+      </Carousel>
+      <button className='buttonknowmore' href='/'>Saiba mais </button>
+    </section>
+  )
+}
+function Conheçanossosplanos () {
+  return (
+    <section className='nossosplanosel'>
+      <Carousel>
+        <Carousel.Item>
+          <img src={group4} alt='Conheça nossos planos com um carrinho rosa de cada lado' className='img-fluid d-block w50 conhecanossosplanosimg'/>
+          <Carousel.Caption>
+            <h1>Assinatura Anual</h1>
+            <div className='planoanualsec'>
+            <h3>Vaga garantida para as 12 primeiras viagens na montanha russa que é o primeiro ano do bebê. Presente de fidelidade.</h3>
+              <li>4 a 5 itens de acordo com a fase do bebê.</li>
+              <li>Roupa temática para mêsversário.</li>
+              <li>1 livro para a mamãe.</li>
+              <li>Conteúdos exclusivos para ampliar a experiência.</li>
+              <li>cupom de 50% de desconto em lojas parceiras. </li>
+              <h2>R$99,00</h2>
+              <p>+ Taxa de entrega</p>
+              </div>
+          </Carousel.Caption>
+
+        </Carousel.Item>
+      </Carousel>
+
+    </section>
   )
 }
 
@@ -30,13 +98,13 @@ function Homeview() {
    <main> 
      
       <section className=''>
-       <a href='/'> <img src={Hero} alt='Criança sorrindo' width={1400} height={600} className='img-fluid
+       <a href='/'> <img src={Hero} alt='Criança sorrindo' width={1400} height={600} className='img-fluid nenem
         '/></a>
       </section>
      
    </main>
-   <p>Como funciona</p>
-   <p>Planos</p>
+   <Comofuncionasection/>
+   <Conheçanossosplanos/>
   </>
   )
 }
