d2a790f04e196818a0e4d05ad6231133a6de86a7
malurmaia <marialuisaribeiromaia@gmail.com>
2022-04-07 21:13:24 -0300

    implementa pagina 404

--- frontend/src/views/NotFound/index.js
+++ frontend/src/views/NotFound/index.js
@@ -1,9 +1,15 @@
+import { Container } from "react-bootstrap";
 import { Header } from "../../components/layout/header";
 
 export function NotFoundView () {
     return(
       <>
       <Header/>
+      <Container className="text-center m-4">
+      <h1>Página não encontrada.</h1>
+      <p>A página que você está procurando não existe ou foi movida.</p>
+      <p>Utilize o menu principal para navegar entre as paginas.</p>
+      </Container>
       {/* Necessário colocar a tag main para manter o footer no final da pagina */}
       {/* <main></main> */}
       </>
