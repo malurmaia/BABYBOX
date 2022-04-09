64e50e512cbcf93326febafa83b736c8a33cbbf1
malurmaia <marialuisaribeiromaia@gmail.com>
2022-04-08 23:13:42 -0300

    adiciona redirects

--- frontend/src/views/home/index.js
+++ frontend/src/views/home/index.js
@@ -2,7 +2,7 @@ import { Header } from '../../components/layout/header'
 import Hero from '../../assets/img/Group-3.jpg'
 import { Comofuncionasection } from '../../components/layout/comofunciona'
 import { Conheçanossosplanos } from '../../components/layout/planos'
-import { Link } from 'react-router-dom'
+
 
 export function Homeview() {
     return (
@@ -10,7 +10,7 @@ export function Homeview() {
      <Header />
      <section> 
         <div className='nenem'>
-         <a as={Link} to='/'> <img src={Hero} alt='Criança sorrindo' width={2200} height={800} className='img-fluid nenem
+         <a href='index.html'> <img src={Hero} alt='Criança sorrindo' width={2200} height={800} className='img-fluid nenem
           '/></a>
         </div>
      </section>
